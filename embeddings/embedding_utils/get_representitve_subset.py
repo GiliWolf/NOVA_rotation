cd300d31b9bc34d94921bb4b7813d59a50cf8855
Gili Wolf <giliwolf1999@gmail.com>
2025-05-26 10:39:28 +0300

    get_representitve_subset works

--- embeddings/embedding_utils/get_representitve_subset.py
+++ embeddings/embedding_utils/get_representitve_subset.py
@@ -42,34 +42,128 @@ def filter_by_labels(labels_df: pd.DataFrame,embeddings_df: pd.DataFrame,paths_d
     return filtered_labels, filtered_embeddings, filtered_paths
 
 
-def filter_and_save(labels_df:pd.DataFrame, embeddings_df:pd.DataFrame,paths_df:pd.DataFrame, marker, batch,  cell_line:str, condition:str, output_dir:str=None):
-    # extract labels and indices
-    filtered_labels= labels_df[
-    (labels_df['cell_line'] == cell_line) &
-    (labels_df['condition'] == condition)
-    ]
+def compute_distances(a1:np.array, a2:np.array, metric='euclidean'):
+    """"Compute all pairwise distances between 2 vectors.
+    parameters:
+        a1: first array 
+        a2: second array
+        mteric: metric to calculate the dist accordinly (defualt: cosine)
+    
+    return:
+        dim1: dimension of a1
+        dim2: dimension of a2
+        flattened_distances: distance matrix 
+    """
+    distances = cdist(a1, a2, metric=metric)  
+    return distances
 
-    if filtered_labels.empty():
-        print(f"No samples found for: {marker} & {batch} & {cell_line} & {condition}")
-        return None
+def get_pairs(flattened_distances, n_pairs, dim2):
+    # get min/max/middle dist pairs
+    sorted_indices = np.argsort(flattened_distances) # sort (ascendingly)
+    pairs = [(idx // dim2, idx % dim2) for idx in sorted_indices] # extract original (matrix) indices 
+    min_pairs = pairs[:n_pairs]
+    max_pairs = pairs[-n_pairs:]
+    middle_start = (len(pairs) // 2) - (n_pairs // 2)
+    middle_pairs = pairs[middle_start:middle_start + n_pairs]
+    
+    return min_pairs, max_pairs, middle_pairs
 
-    # extract data 
-    filtered_embeddings = embeddings_df.iloc[filtered_labels.index]
-    filtered_paths = paths_df.iloc[filtered_labels.index]
+def visualize_pairs(distances, flattened_distances, min_pairs, max_pairs, middle_pairs, metric, output_dir=None):
+    # Distances of selected pairs
+    selected_min_distances = [distances[i, j] for i, j in min_pairs]
+    selected_max_distances = [distances[i, j] for i, j in max_pairs]
+    selected_middle_distances = [distances[i, j] for i, j in middle_pairs]
 
-    # save
+
+    plt.figure(figsize=(10, 6))
+    plt.hist(flattened_distances, bins=50, alpha=0.7, color='blue', label='All distances')
+
+    # Plot vertical lines
+    for d in selected_min_distances:
+        plt.axvline(d, color='green', linestyle='--', linewidth=1)
+
+    for d in selected_max_distances:
+        plt.axvline(d, color='red', linestyle='--', linewidth=1)
+
+    for d in selected_middle_distances:
+        plt.axvline(d, color='yellow', linestyle='--', linewidth=1)
+
+    plt.xlabel(f"{metric} Distance")
+    plt.ylabel("Count")
+    plt.title("Distribution of All Pairwise Distances")
+    plt.legend()
+
     if output_dir:
-        filtered_labels["full_label"].to_csv(os.path.join(output_dir, f"{cell_line}_{condition}_labels.csv"), index=False)
-        np.save(os.path.join(output_dir, f"{cell_line}_{condition}_embedding.npy"),  filtered_embeddings.to_numpy())
-        filtered_paths.to_csv(os.path.join(output_dir, f"{cell_line}_{condition}_paths.csv"), index=False)
+        plt.savefig(os.path.join(output_dir, f"{metric}_distance_distribution.png"))
+        plt.close()
+    else:
+        plt.show()
 
-    return filtered_labels, filtered_embeddings, filtered_paths
 
-def main(output_folder_path:str, config_path_data:str):
+def extract_subset(marker_labels, marker_embeddings, marker_paths, metric, num_pairs, set_type, data_config, output_dir):
+                filtered_labels_c1, filtered_embeddings_c1, filtered_paths_c1  = filter_by_labels(marker_labels, marker_embeddings, marker_paths, {'cell_line':data_config.CELL_LINES[0], 'condition':data_config.CONDITIONS[0]})
+                filtered_labels_c2, filtered_embeddings_c2, filtered_paths_c2 = filter_by_labels(marker_labels, marker_embeddings, marker_paths, {'cell_line':data_config.CELL_LINES[0], 'condition':data_config.CONDITIONS[1]})
+                
+                #convert to nparraay
+                filtered_embeddings_c1 = np.array(filtered_embeddings_c1)
+                filtered_embeddings_c2 = np.array(filtered_embeddings_c2)
+                
+                num_samples_c1, num_samples_c2 = len(filtered_embeddings_c1), len(filtered_embeddings_c2)
+                
+                # Compute all pairwise distances between condition 1 and 2
+                distances = compute_distances(filtered_embeddings_c1, filtered_embeddings_c2, metric)
+                flattened_distances = distances.flatten()
+                
+                # extract min/max/middle pairs
+                min_pairs, max_pairs, middle_pairs = get_pairs(flattened_distances, num_pairs, dim2 = num_samples_c2)
+
+                # Combine all pairs with their labels
+                labeled_pairs = [
+                    ("min", pair) for pair in min_pairs
+                ] + [
+                    ("middle", pair) for pair in middle_pairs
+                ] + [
+                    ("max", pair) for pair in max_pairs
+                ]
+
+                # Get indices for condition 1 and 2 samples
+                c1_indices = np.array(list(set([i for (_, (i, j)) in labeled_pairs])))
+                c2_indices = np.array(list(set([j for (_, (i, j)) in labeled_pairs])))
+                print(f"Selected {len(c1_indices)} {data_config.CONDITIONS[0]} samples and {len(c2_indices)} {data_config.CONDITIONS[1]} samples.")
+
+                # Save distances
+                distances_df = pd.DataFrame()
+                distances_df["pair_type"] = [label for (label, (i, j)) in labeled_pairs]
+                distances_df[f"{metric}_distance"] = [distances[i, j] for (label, (i, j)) in labeled_pairs]
+                distances_df[f"path_{data_config.CONDITIONS[0]}"] = [filtered_paths_c1.iloc[i]["Path"] for (label, (i, j)) in labeled_pairs]
+                distances_df[f"path_{data_config.CONDITIONS[1]}"] = [filtered_paths_c2.iloc[j]["Path"] for (label, (i, j)) in labeled_pairs]
+                distances_df.to_csv(os.path.join(output_dir, "distances.csv"), index=False)
+
+                # extract embeding,labels and paths values
+                c1_embeddings = filtered_embeddings_c1[c1_indices]
+                c1_labels = filtered_labels_c1.iloc[c1_indices]
+                c1_paths = filtered_paths_c1.iloc[c1_indices]
+
+                c2_embeddings = filtered_embeddings_c2[c2_indices]
+                c2_labels = filtered_labels_c2.iloc[c2_indices]
+                c2_paths = filtered_paths_c2.iloc[c2_indices]
+
+                set_embeddings = np.concatenate([c1_embeddings, c2_embeddings], axis=0)
+                set_labels = pd.concat([c1_labels, c2_labels], axis=0).reset_index(drop=True)
+                set_paths= pd.concat([c1_paths, c2_paths], axis=0).reset_index(drop=True)
+
+                # Save npy files
+                np.save(os.path.join(output_dir, f"{set_type}.npy"), set_embeddings)
+                np.save(os.path.join(output_dir, f"{set_type}_labels.npy"), set_labels["full_label"].values)
+                np.save(os.path.join(output_dir, f"{set_type}_paths.npy"), np.array(set_paths["Path"].values, dtype=str))
+
+                visualize_pairs(distances, flattened_distances, min_pairs, max_pairs, middle_pairs, metric, output_dir = output_dir)    
+
+
+def main(output_folder_path:str, config_path_data:str, metric:str, num_pairs:int):
 
     data_config:DatasetConfig = load_config_file(config_path_data, "data")
     data_config.OUTPUTS_FOLDER = output_folder_path
-
 
     home_dir = os.getenv("HOME")
     emb_out_dir = "NOVA_rotation/embeddings/embedding_output"
@@ -110,18 +204,20 @@ def main(output_folder_path:str, config_path_data:str):
                     continue
                 temp_output_dir = os.path.join(output_folder_path, data_config.EXPERIMENT_TYPE, batch, marker)
                 os.makedirs(temp_output_dir, exist_ok=True)
-                filtered_labels_c1, filtered_embeddings_c1, filtered_paths_c1  = filter_by_labels(marker_labels, marker_embeddings, marker_paths, {'cell_line':data_config.CELL_LINES[0], 'condition':data_config.CONDITIONS[0]})
-                filtered_labels_c2, filtered_embeddings_c2, filtered_paths_c2 = filter_by_labels(marker_labels, marker_embeddings, marker_paths, {'cell_line':data_config.CELL_LINES[0], 'condition':data_config.CONDITIONS[1]})
 
-            
+                extract_subset(marker_labels, marker_embeddings, marker_paths, metric, num_pairs, set_type, data_config, temp_output_dir)
+                
+                            
 if __name__ == "__main__":
     try:
-        if len(sys.argv) < 3:
-            raise ValueError("Invalid arguments. Must supply outputs folder path, data config.")
+        if len(sys.argv) < 5:
+            raise ValueError("Invalid arguments. Must supply outputs folder path, data config, metric and num of pairs")
         outputs_folder_path = sys.argv[1]
         config_path_data = sys.argv[2]
+        metric = sys.argv[3]
+        num_pairs = int(sys.argv[4])
 
-        main(outputs_folder_path, config_path_data)
+        main(outputs_folder_path, config_path_data,  metric, num_pairs)
         
     except Exception as e:
         print(e)
